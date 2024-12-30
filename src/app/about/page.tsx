09f5b1c455a0aa051b0d0204965d7e3c9b24ef46
saad <sk5908774@gmail.com>
2024-12-31 02:22:54 +0500

    update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,22 @@
+import Head from 'next/head'
 import React from 'react'
 
 const page = () => {
   return (
+    <>
+      <Head>
+        <title>About Us - Your Website Name</title>
+        <meta name="description" content="Learn more about us on this page." />
+        <meta property="og:title" content="About Us - Your Website Name" />
+        <meta property="og:description" content="Learn more about us on this page." />
+        <meta property="og:url" content="https://meta-testing-delta.vercel.app/about" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://meta-testing-delta.vercel.app/logo.webp" />
+      </Head>
     <div>
       About
     </div>
+    </>
   )
 }
 
